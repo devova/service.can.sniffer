fc664c63ed608de0324c1adcbda2409cebcd06a0
Volodymyr Trotsyshyn <devova@gmail.com>
2016-09-20 22:11:58 +0300

    added depended modules

---
+++ lib/utils.py
@@ -0,0 +1,8 @@
+import xbmcaddon
+
+
+ADDON = xbmcaddon.Addon()
+ADDON_ID = ADDON.getAddonInfo('id').decode("utf-8")
+ADDON_ICON = ADDON.getAddonInfo('icon').decode("utf-8")
+ADDON_NAME = ADDON.getAddonInfo('name').decode("utf-8")
+ADDON_PATH = ADDON.getAddonInfo('path').decode("utf-8")
--- service.py
+++ service.py
@@ -2,30 +2,47 @@ import sys
 import os
 MODULE_PATH = os.path.dirname(__file__)
 sys.path.append(MODULE_PATH)
-sys.path.append(os.path.join(MODULE_PATH, 'pycharmdebug'))
+sys.path.append(os.path.join(MODULE_PATH, 'eggs'))
+sys.path.append('/Users/devova/PycharmProjects/citrocan/app')
 
 if os.environ.get('PYTHON_DEBUG_HOST'):
     try:
+        sys.path.append(os.path.join(MODULE_PATH, 'pycharmdebug'))
         from pycharmdebug import pydevd
         pydevd.settrace('localhost', port=5678, stdoutToServer=True, stderrToServer=True)
     except ImportError:
         pass
 
 import xbmc
-import xbmcaddon
 import xbmcgui
+import decoder
+
+from lib import utils, socket_server
+# from decoder.telnet import TelnetDecoder
+
+commport = 51456
 
 if __name__ == '__main__':
+    # decoder = TelnetDecoder(decoder.DEFAULT_DECODERS, proxy_attributes=True)
+    # decoder.connect('127.0.0.1', 8333)
+    socket_server.start()
+    addon = utils.ADDON
+    addonname = utils.ADDON_NAME
+
+    host = 'localhost'
+
+
+    # xbmcgui.Dialog().ok(addonname, 'Start', 'Start', 'Start')
+
+
     monitor = xbmc.Monitor()
  
     while not monitor.abortRequested():
-        addon = xbmcaddon.Addon()
-        addonname = addon.getAddonInfo('name')
 
         line2 = 'Test'
 
         xbmcgui.Dialog().notification(addonname, line2)
-
+        # xmbc.setInfoLabel('Citroen.Test', addonname)
         # Sleep/wait for abort for 10 seconds
         if monitor.waitForAbort(10):
             # Abort was requested while waiting. We should exit
